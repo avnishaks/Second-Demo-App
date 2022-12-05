8717b4b41af9525def0f807c4dd81e92abe54a5d
Avnish Kumar <avnishgolu927@gmail.com>
2022-12-05 23:29:38 +0530

    Second App

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Founding Member of Travel and Tech Company</Text>
       <StatusBar style="auto" />
     </View>
   );
